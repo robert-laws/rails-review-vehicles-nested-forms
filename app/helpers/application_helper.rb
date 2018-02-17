b0fdd3a30d6ff5581519c532835928c0ca4c908a
Robert Laws <roblaws@me.com>
2018-02-17 20:40:49 +0300

    begin work on rails helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_time
+    content_for :current_time, "Today is #{DateTime.now.strftime("%A, %B %e, %Y")}"
+  end
+
+  def title(text)
+    content_for :title, text
+  end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def event_date_time(event)
+    event.event_date.strftime("%A, %b %e, at %l:%M %p")
+  end
 end
